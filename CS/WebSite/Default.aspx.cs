f41b24044eacde3995369fcfdbb190b84caf0ecf
DevExpressExampleBot <fakeEmail>
2022-12-07 14:41:56 +0400

    Source auto update [skip ci]

--- CS/WebSite/Default.aspx.cs
+++ CS/WebSite/Default.aspx.cs
@@ -7,7 +7,7 @@ using System.Web.UI.WebControls;
 
 public partial class _Default : System.Web.UI.Page
 {
-    protected void ASPxGridView1_HtmlDataCellPrepared(object sender, DevExpress.Web.ASPxGridView.ASPxGridViewTableDataCellEventArgs e) {
+    protected void ASPxGridView1_HtmlDataCellPrepared(object sender, DevExpress.Web.ASPxGridViewTableDataCellEventArgs e) {
         String colIdx = "col_idx_" + e.DataColumn.Index.ToString();
         String rowIdx = "row_idx_" + e.VisibleIndex.ToString();
         e.Cell.Attributes.Add("onmouseover", "OnCellOver(this, '" + colIdx + "', '" + rowIdx + "');");
--- VB/WebSite/Default.aspx.vb
+++ VB/WebSite/Default.aspx.vb
@@ -8,7 +8,7 @@ Imports System.Web.UI.WebControls
 
 Partial Public Class _Default
 	Inherits System.Web.UI.Page
-	Protected Sub ASPxGridView1_HtmlDataCellPrepared(ByVal sender As Object, ByVal e As DevExpress.Web.ASPxGridView.ASPxGridViewTableDataCellEventArgs)
+	Protected Sub ASPxGridView1_HtmlDataCellPrepared(ByVal sender As Object, ByVal e As DevExpress.Web.ASPxGridViewTableDataCellEventArgs)
 		Dim colIdx As String = "col_idx_" & e.DataColumn.Index.ToString()
 		Dim rowIdx As String = "row_idx_" & e.VisibleIndex.ToString()
 		e.Cell.Attributes.Add("onmouseover", "OnCellOver(this, '" & colIdx & "', '" & rowIdx & "');")
